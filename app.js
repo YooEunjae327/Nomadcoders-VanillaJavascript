22547a2249cd27a786de6080df00ae648c5cda33
tomato <tomato6726@gmail.com>
2022-04-04 16:59:59 +0900

    .

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-alert("hi")
+const h1 = document.querySelector("div.hello:first-child h1")
+
+const handleTitleClick = () => {
+  h1.style.color = "blue"
+}
+
+const handleMouseEnter = () => {
+  h1.innerText = "Mouse is here!"
+}
+const handleMouseLeave = () => {
+  h1.innerText = "Mouse is gone!"
+}
+
+const handleWindowResize = () => {
+  document.body.style.backgroundColor = "tomato"
+}
+
+const handleWindowCopy = () => {
+  alert("copier!")
+}
+
+const handleWindowOffline = () => {
+  alert("SOS no WIFI")
+}
+
+const handleWindowOnline = () => {
+  alert("All Goood")
+}
+
+h1.addEventListener("click", handleTitleClick)
+h1.addEventListener("mouseenter", handleMouseEnter)
+h1.addEventListener("mouseleave", handleMouseLeave)
+
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
