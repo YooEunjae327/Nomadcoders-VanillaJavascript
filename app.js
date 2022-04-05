306e37e62360af9d0857f9061447057d809a339c
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-04-05 21:38:03 +0900

    .

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
 
-const onLoginSubmit = () => {
-  const username = loginInput.value
-  console.log(username)
+const onLoginSubmit = (event) => {
+  event.preventDefault()
+  console.log(loginInput.value)
 }
 loginForm.addEventListener('submit', onLoginSubmit)
 
