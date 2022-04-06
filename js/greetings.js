cc7164314fb94613cb9047799fea44fb83065437
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-04-06 13:12:21 +0900

    .

--- js/greetings.js
+++ js/greetings.js
@@ -1,11 +1,26 @@
 const loginForm = document.querySelector('#login-form')
 const loginInput = document.querySelector('#login-form input')
+const greeting = document.querySelector('#greeting')
+
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
 
 const onLoginSubmit = (event) => {
   event.preventDefault()
-  console.log(loginInput.value)
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value
+  localStorage.setItem(USERNAME_KEY, username)
+  greeting.innerHTML = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME)
 }
-loginForm.addEventListener('submit', onLoginSubmit)
+
+const saveUsername = localStorage.getItem(USERNAME_KEY)
+console.log(saveUsername)
+
+if (saveUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', onLoginSubmit)
+}
 
 // const h1 = document.querySelector("div.hello:first-child h1")
 
